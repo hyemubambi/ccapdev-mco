93e621deda3237a04c5d8446c68ad14c155c366e
Kyle-Trinos <134774181+Kyle-Trinos@users.noreply.github.com>
2023-11-14 02:16:00 +0800

    Add files via upload

---
+++ controllers/searchController.js
@@ -0,0 +1,25 @@
+// Your search controller logic
+const Search = require('../models/condo');
+
+async function searchController(req, res) {
+    try {
+        const {keyword} = req.query;
+
+        if (!keyword || keyword.trim() === '') {
+            return;
+        }
+
+        const condos = await Search.find({ cName: { $regex: new RegExp(keyword, 'i') } }).select('cName description lRange hRange nReviews rating photo');
+
+        console.log('Search Keyword:', keyword);
+        console.log('Number of Matching Condos:', condos.length);
+        console.log('Search Results:', condos);
+
+        res.render('searchCondo', { condos });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+module.exports = searchController;
