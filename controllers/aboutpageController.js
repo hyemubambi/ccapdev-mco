d299cc1dcfade6ad6d79ce5e531e8ea8f107ab00
Elaine-21 <101459900+Elaine-21@users.noreply.github.com>
2023-11-14 13:00:16 +0800

    Add files via upload

---
+++ controllers/aboutpageController.js
@@ -0,0 +1,11 @@
+
+async function aboutpageController(req, res) {
+    try {
+        res.render('aboutpage');
+    } catch {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+module.exports = aboutpageController;
