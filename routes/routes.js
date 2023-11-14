b6178f03402f4c59272272b10a3e06cfcbc9e78d
Elaine-21 <101459900+Elaine-21@users.noreply.github.com>
2023-11-14 13:02:00 +0800

    Add files via upload

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
@@ -12,13 +13,18 @@ router.use(express.urlencoded( { extended : true } ));
 // import controllers
 const homepageController = require('../controllers/homepageController');
 const searchController = require('../controllers/searchController');
+const condolistController = require('../controllers/condolistController');
+const aboutpageController = require('../controllers/aboutpageController');
+const condoprofileController = require('../controllers/condoprofileController');
 
 // GET FUNCTIONS
 
 router.get('/', homepageController);
 router.get('/homepage', homepageController);
 router.get('/searchResult', searchController);
-
+router.get('/condolist', condolistController);
+router.get('/aboutpage', aboutpageController);
+router.get('/condo/profile', condoprofileController);
 
 router.use((req, res, next) => {
     const err = new Error('Not Found');
