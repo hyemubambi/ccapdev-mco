d9c810a53c45ecfe3bd70a69b8423849c4ff9eb6
Elaine-21 <101459900+Elaine-21@users.noreply.github.com>
2023-11-14 13:01:12 +0800

    Add files via upload

--- public/js/condolist.js
+++ public/js/condolist.js
@@ -20,9 +20,9 @@ $(document).ready(function(){
         var list = $("#condo-sorting");
         var listitems = list.children(".recommended-condo").get();
         listitems.sort(function(a,b){
-            var compA = $(a).attr('id').toUpperCase();
-            var compB = $(b).attr('id').toUpperCase();
-            return (compA < compB) ? -1: (compA > compB) ? 1:0;
+            var compA = parseFloat($(a).attr('id'));
+            var compB = parseFloat($(b).attr('id'));
+            return (compA > compB) ? -1: (compA < compB) ? 1:0;
         });
         $.each(listitems, function(idx, itm){
             list.append(itm);
@@ -34,9 +34,23 @@ $(document).ready(function(){
         var list = $("#condo-sorting");
         var budget = list.find(".budget").get();
         budget.sort(function(a,b){
-            var compA = $(a).attr('id').toUpperCase();
-            var compB = $(b).attr('id').toUpperCase();
-            return (compA < compB) ? -1: (compA > compB) ? 1:0;
+            var compA = parseFloat($(a).attr('id'));
+            var compB = parseFloat($(b).attr('id'));
+            console.log("A: " + compA);
+            console.log("B: " + compB);
+            var res = (compA < compB) ? -1: (compA > compB) ? 1:0;
+            console.log("Lower: " + res);
+
+            if(res === 0){
+                var highA = parseFloat($(a).next().attr('class'));
+                var highB = parseFloat($(b).next().attr('class'));
+                console.log("High A: " + highA);
+                console.log("High B: " + highB);
+                var res = (highA < highB) ? -1: (highA > highB) ? 1:0;
+                console.log("Upper: " + res);
+            }
+
+            return res;
         });
         $.each(budget, function(idx, itm){
             list.append($(itm).parents(".recommended-condo"));
