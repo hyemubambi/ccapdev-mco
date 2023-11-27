3a4f1147aaaae26919f0a67639a1cd90c93fce53
Janina Bongon <janina.alexia@gmail.com>
2023-11-28 03:19:11 +0800

    added passport and session to index.js, installment of new npm packages

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+// initialization
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const routes = require('./routes/routes.js');
 const db = require('./models/db.js');
@@ -15,18 +19,48 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// connect to mongodb database
+db.connect();
+
+// session middleware
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+// initialize passport and session management
+app.use(passport.initialize());
+app.use(passport.session());
+
+// import and configure your user model
+const User = require('../models/user.js');
+
+// use passport-local-mongoose plugin for user schema
+User.plugin(passportLocalMongoose);
+
+// configure passport strategies
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// routes
 app.use('/', routes);
+
+// error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(err.status || 500).render('error', { err, error: err.message });
 });
 
-db.connect();
 
-//just call the function here
+// populating data functions
 // addData.populateCondo();
 // addData.populateUsers();
 
+// start the server
 app.listen(process.env.PORT || 3000, function () {
     console.log("Server started on port 3000");
 });
