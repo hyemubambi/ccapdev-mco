45dc18f99f62abb3e1aba56cd33de030d3ec1157
Janina Bongon <janina.alexia@gmail.com>
2023-11-30 02:21:00 +0800

    working login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,24 +1,42 @@
-const db = require('../models/db');
+const passport = require('passport');
 const User = require('../models/user');
+
 async function loginController(req, res) {
     try {
-        res.render('login');
+        res.render('login', {
+            email: '',
+            password: '',
+            error:'',
+        });
     } catch {
         console.error(error);
         res.status(500).send('Internal Server Error');
     }
 }
+
 async function postLogin(req, res){
 
     try {
-        var email = req.body.Email;
-        var password = req.body.password;
-        const user = await User.findOne({email: email})
-        if(user){
-            if(user.password === password){
-                res.redirect(`/homepage?user=${user.username}`);
+        
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password,
+        });
+        
+        req.login(user, function (err) {
+            if (err) {
+              console.log(err);
+              res.render('login', {
+                username: req.body.username,
+                password: req.body.password,
+                error : 'Incorrect username/password'
+            });
+            } else {
+              passport.authenticate("local")(req, res, function () {
+                res.redirect(`/userprofile?username=${user.username}`);
+              });
             }
-        }
+        });
 
     } catch (error) {
         console.error('Error:', error);
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
 
 const routes = require('./routes/routes.js');
 const db = require('./models/db.js');
@@ -35,9 +36,37 @@ app.use(passport.session());
 const User = require('./models/user.js');
 
 // configure passport strategies
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.use(User.createStrategy());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+passport.use(new LocalStrategy({
+    usernameField: 'username', // Assuming 'email' is the field for username
+    passwordField: 'password' // Assuming 'password' is the field for the password
+ },
+ function(username, password, done) {
+    // Logic to find and authenticate user based on email and password
+    User.findOne({ username: username })
+        .then(user => {
+            if (!user || !user.validPassword(password)) { // Assuming a method to check password validity
+                return done(null, false, { message: 'Incorrect username or password.' });
+            }
+            return done(null, user);
+        })
+        .catch(err => done(err));
+ }));
+ 
+ passport.serializeUser(function(user, done) {
+    done(null, user.id); // Using the user's ID for serialization
+ });
+ 
+ passport.deserializeUser(function(id, done) {
+    User.findById(id)
+        .then(user => done(null, user))
+        .catch(err => done(err));
+ });
+ 
+
 
 // routes
 app.use('/', routes);
