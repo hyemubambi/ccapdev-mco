3d7ff05e599dd453c2f5d7c6864d32e541242297
Kyle-Trinos <134774181+Kyle-Trinos@users.noreply.github.com>
2023-12-04 20:18:40 +0800

    Edit condo and delete condo

--- controllers/condopanelController.js
+++ controllers/condopanelController.js
@@ -28,7 +28,58 @@ async function renderCondoPanel(req, res) {
         res.status(500).send('Internal Server Error');
     }
 }
+async function editCondoProfile(req, res) {
+    const { cName } = req.body;
+    const { name, description, budgetLower, budgetUpper, image } = req.body;
+    let user = null;
+  
+    // Get the uploaded file information
+    const uploadedFile = "uploads/" + req.file.filename;
+  
+    // Find the condo by its cName and update it
+    const condo = await Condo.findOneAndUpdate({ cName }, { name, description, lRange: budgetLower, hRange: budgetUpper, photo: uploadedFile }, { new: true });
+  
+    // Fetch the list of condos
+    const condos = await Condo.find().sort({cName: 1});
+  
+    loggedIn = true;
+    user = req.session.user;
+  
+    if (!condo) {
+        return res.status(404).send('Condo not found');
+    }
+  
+    res.render('condopanel', {
+        condos,
+        loggedIn,
+        user
+    });
+  }
+  
+ 
 
-module.exports = {
-    renderCondoPanel
+  async function deleteCondoProfile(req, res) {
+    const { cName } = req.body;
+    let user = null;
+    const condos = await Condo.find().sort({cName: 1});
+    loggedIn = true;
+    user = req.session.user;
+    // Find the condo by its cName and remove it
+    const condo = await Condo.findOneAndDelete({ cName });
+   
+    if (!condo) {
+        return res.status(404).send('Condo not found');
+    }
+   
+    res.render('condopanel', {
+        condos,
+        loggedIn,
+        user
+    });
+   }
+   
+   module.exports = {
+    renderCondoPanel,
+    editCondoProfile,
+    deleteCondoProfile
 };
