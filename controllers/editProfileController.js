af5d01690df678e471c3fb91d41f5f7ca03332fd
Elaine-21 <101459900+Elaine-21@users.noreply.github.com>
2023-12-04 21:54:01 +0800

    Add files via upload
    added controllers for editing profile

--- controllers/editProfileController.js
+++ controllers/editProfileController.js
@@ -1,4 +1,7 @@
 const User = require('../models/user.js');
+const Condo = require('../models/condo.js');
+const Review = require('../models/review.js');
+const Comment = require('../models/comment.js');
 
 async function editProfileController(req, res) {
     try {
@@ -24,5 +27,97 @@ async function editProfileController(req, res) {
         res.status(500).send('Internal Server Error');
     }
 }
+async function postEditPersonalInfo(req, res){
+    try{
+        const {fName, lName} = req.body;
+        const userID = req.params.userID;
 
-module.exports = editProfileController;
+
+        const updateUser = await User.findByIdAndUpdate(userID, {fName, lName}, {new:true});
+        if (!updateUser) {
+            // Handle the case where the user is not found
+            return res.status(404).send('User not found');
+        }
+        loggedIn = true;
+        const user = req.session.user;
+        const username = user.username;
+        const userProfile = await User.findOne({ username: username });
+        const reviews = await Review.find({ username: username });
+        const reviewIds = reviews.map(review => review._id);
+        const comments = await Comment.find({ review: { $in: reviewIds } });
+        res.render('userprofile', {
+            loggedIn, 
+            user, 
+            userProfile, 
+            reviews,
+            comments});
+    }catch (error){
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+    
+}
+async function postEditAccountDetails(req, res){
+    try{
+        const {username} = req.body;
+        const userID = req.params.userID;
+
+
+        const updateUser = await User.findByIdAndUpdate(userID, {username}, {new:true});
+        if (!updateUser) {
+            // Handle the case where the user is not found
+            return res.status(404).send('User not found');
+        }
+        loggedIn = true;
+        const user = req.session.user;
+        const userProfile = await User.findOne({ username: username });
+        const reviews = await Review.find({ username: username });
+        const reviewIds = reviews.map(review => review._id);
+        const comments = await Comment.find({ review: { $in: reviewIds } });
+        res.render('userprofile', {
+            loggedIn, 
+            user, 
+            userProfile, 
+            reviews,
+            comments});
+    }catch (error){
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+// async function postAdditionalInfo(req, res){
+//     try{
+//         const {bio, uCondo} = req.body;
+//         const userID = req.params.userID;
+
+
+//         const updateUser = await User.findByIdAndUpdate(userID, {bio, uCondo}, {new:true});
+//         if (!updateUser) {
+//             // Handle the case where the user is not found
+//             return res.status(404).send('User not found');
+//         }
+//         loggedIn = true;
+//         const user = req.session.user;
+//         const username = user.username;
+//         const userProfile = await User.findOne({ username: username });
+//         const reviews = await Review.find({ username: username });
+//         const reviewIds = reviews.map(review => review._id);
+//         const comments = await Comment.find({ review: { $in: reviewIds } });
+//         res.render('userprofile', {
+//             loggedIn, 
+//             user, 
+//             userProfile, 
+//             reviews,
+//             comments});
+//     }catch (error){
+//         console.error(error);
+//         res.status(500).json({ error: 'Internal Server Error' });
+//     }
+// }
+
+module.exports = {
+    editProfileController,
+    postEditPersonalInfo,
+    postEditAccountDetails,
+};
